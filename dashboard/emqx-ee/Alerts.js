aa650751a4e58006d23ad65a278bae631c9e912b
wivwiv <bigpar@qq.com>
2020-08-27 13:44:24 +0800

    Update i18n dashboard

--- dashboard/emqx-ee/Alerts.js
+++ dashboard/emqx-ee/Alerts.js
@@ -1,109 +1,108 @@
-export default
-{
+export default {
   __not: [],
   alarm: {
     zh: '告警',
-    jp: '告警',
+    ja: 'アラーム',
     en: 'Alarm',
   },
   currentAlarm: {
     zh: '当前告警',
-    jp: '当前告警',
+    ja: 'カレントアラーム',
     en: 'Current Alarm',
   },
   historyAlarm: {
     zh: '历史告警',
-    jp: '历史告警',
+    ja: 'アラーム履歴',
     en: 'History Alarm',
   },
   node: {
     zh: '节点',
-    jp: '节点',
+    ja: 'ノード',
     en: 'Node',
   },
   type: {
     zh: '类型',
-    jp: '类型',
+    ja: 'タイプ',
     en: 'Type',
   },
   triggerThreshold: {
     zh: '触发阈值',
-    jp: '触发阈值',
+    ja: 'トリガー閾値',
     en: 'Trigger threshold',
   },
   triggerNode: {
     zh: '触发节点',
-    jp: '触发节点',
+    ja: 'トリガーノード',
     en: 'Trigger Node',
   },
   alarmType: {
     zh: '告警类型',
-    jp: '告警类型',
+    ja: 'アラームタイプ',
     en: 'Alarm Type',
   },
   alarmLevel: {
     zh: '告警级别',
-    jp: '告警级别',
+    ja: 'アラームレベル',
     en: 'Alarm level',
   },
   system: {
     zh: '系统',
-    jp: '系统',
+    ja: 'システム',
     en: 'System',
   },
   describe: {
     zh: '描述',
-    jp: '描述',
+    ja: '詳細',
     en: 'Description',
   },
   state: {
     zh: '状态',
-    jp: '状态',
+    ja: 'ステータス',
     en: 'State',
   },
   alarmValue: {
     zh: '告警值',
-    jp: '告警值',
+    ja: 'アラーム値',
     en: 'Alarm value',
   },
   alarmTime: {
     zh: '告警时间',
-    jp: '告警时间',
+    ja: 'アラーム時刻',
     en: 'Create At',
   },
   clearanceTime: {
     zh: '清除时间',
-    jp: '清除时间',
+    ja: 'クリアランスタイム',
     en: 'Clearance time',
   },
   endingTime: {
     zh: '结束时间',
-    jp: '结束时间',
+    ja: '終了時間',
     en: 'Ending time',
   },
   delete: {
     zh: '删除',
-    jp: '删除',
+    ja: '削除',
     en: 'Delete',
   },
   alarms: {
     zh: '告警列表',
-    jp: '告警列表',
+    ja: 'アラームリスト',
     en: 'Alarms',
   },
   normal: {
     zh: '正常',
-    jp: '正常',
+    ja: '異常なし',
     en: 'normal',
   },
   abnormal: {
     zh: '异常',
-    jp: '异常',
+    ja: '異常あり',
     en: 'Alarm',
   },
   second: {
     zh: '次',
-    jp: '次',
+    ja: '次の',
     en: 'second',
   },
 }
