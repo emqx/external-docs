d977f8f9631d74dbc6eb2da4a22a464775633257
duyuxuan <339188534@qq.com>
2020-08-27 23:12:57 +0900

    add Japanese translations of mqttx

---
+++ mqttx/connections.ts
@@ -0,0 +1,287 @@
+export default {
+  connections: {
+    zh: '连接',
+    en: 'Connections',
+    jp: '接続'
+  },
+  newConnections: {
+    zh: '新建连接',
+    en: 'New Connection',
+    jp: '新規接続'
+  },
+  search: {
+    zh: '搜索',
+    en: 'Search',
+    jp: '検索'
+  },
+  searchByTopic: {
+    zh: '按主题搜索',
+    en: 'Search by Topic',
+    jp: 'トピックで検索'
+  },
+  topicCopied: {
+    zh: '复制成功!',
+    en: 'Topic copied!',
+    jp: 'コピーが成功しました'
+  },
+  clearHistory: {
+    zh: '清除历史记录',
+    en: 'Clear Histroy',
+    jp: '履歴データの削除'
+  },
+  notConnect: {
+    zh: '客户端未连接',
+    en: 'Client not connected',
+    jp: 'クライアント未接続'
+  },
+  disconnect: {
+    zh: '断开连接',
+    en: 'Disconnect',
+    jp: '接続を切る'
+  },
+  disconnected: {
+    zh: '已断开连接',
+    en: 'Disconnected',
+    jp: '接続切れ'
+  },
+  deleteConnect: {
+    zh: '删除连接',
+    en: 'Delete Connection',
+    jp: '接続削除'
+  },
+  all: {
+    zh: '全部',
+    en: 'All',
+    jp: '全て'
+  },
+  received: {
+    zh: '已接收',
+    en: 'Received',
+    jp: '受信済み'
+  },
+  published: {
+    zh: '已发送',
+    en: 'Published',
+    jp: '送信済み'
+  },
+  writeMsg: {
+    zh: '请输入消息',
+    en: 'Write a message',
+    jp: 'メッセージを入力してください'
+  },
+  subscriptions: {
+    zh: '订阅列表',
+    en: 'Subscriptions',
+    jp: 'サブスクリプションリスト'
+  },
+  subscription: {
+    zh: '订阅',
+    en: 'subscription',
+    jp: 'サブスクリプション'
+  },
+  newSubscription: {
+    zh: '添加订阅',
+    en: 'New Subscription',
+    jp: 'サブスクリプション追加'
+  },
+  subFailed: {
+    zh: '订阅失败',
+    en: 'Subscribe Failed',
+    jp: 'サブスクリプションが失敗しました'
+  },
+  connected: {
+    zh: '已连接',
+    en: 'Connected',
+    jp: '接続済み'
+  },
+  connectFailed: {
+    zh: '连接失败',
+    en: 'Connect Failed',
+    jp: '接続に失敗しました'
+  },
+  reconnect: {
+    zh: '正在重连',
+    en: 'Reconnecting',
+    jp: '再接続中'
+  },
+  connectBtn: {
+    zh: '连 接',
+    en: 'Connect',
+    jp: '接 続'
+  },
+  disconnectedBtn: {
+    zh: '断开连接',
+    en: 'Disconnect',
+    jp: '接続を切る'
+  },
+  connectionExists: {
+    zh: '连接数据已存在',
+    en: 'Connection already exists',
+    jp: '接続がすでに存在しました'
+  },
+  brokerIP: {
+    zh: '服务器地址',
+    en: 'Host',
+    jp: 'ホスト'
+  },
+  brokerPort: {
+    zh: '端口',
+    en: 'Port',
+    jp: 'ポート'
+  },
+  certType: {
+    zh: '证书类型',
+    en: 'Certificate',
+    jp: '証明書タイプ'
+  },
+  name: {
+    zh: '名称',
+    en: 'Name',
+    jp: '名前'
+  },
+  username: {
+    zh: '用户名',
+    en: 'Username',
+    jp: 'ユーザー名'
+  },
+  password: {
+    zh: '密码',
+    en: 'Password',
+    jp: 'パスワード'
+  },
+  ca: {
+    zh: 'CA 文件',
+    en: 'CA File',
+    jp: 'CA ファイル'
+  },
+  cert: {
+    zh: '客户端证书',
+    en: 'Client Certificate File',
+    jp: 'クライアント証明書'
+  },
+  key: {
+    zh: '客户端 key 文件',
+    en: 'Client key file',
+    jp: 'クライアント キー ファイル'
+  },
+  connectionTimeout: {
+    zh: '连接超时时长',
+    en: 'Connect Timeout',
+    jp: '接続タイムアウト'
+  },
+  cleanSession: {
+    zh: '清除会话',
+    en: 'Clean Session',
+    jp: 'セッションクリア'
+  },
+  autoReconnect: {
+    zh: '自动重连',
+    en: 'Auto Reconnect',
+    jp: '自動再接続'
+  },
+  mqttVersion: {
+    zh: 'MQTT 版本',
+    en: 'MQTT Version',
+    jp: 'MQTT バージョン'
+  },
+  sessionExpiryInterval: {
+    zh: '会话过期时间',
+    en: 'Session Expiry Interval',
+    jp: 'セッション有効期限'
+  },
+  receiveMaximum: {
+    zh: '接收最大数值',
+    en: 'Receive Maximum',
+    jp: '最大受信数'
+  },
+  topicAliasMaximum: {
+    zh: '主题别名最大值',
+    en: 'Topic Alias Maximum',
+    jp: 'トピックエイリアスの最大値'
+  },
+  requestResponseInformation: {
+    zh: '请求响应信息',
+    en: 'Request Response Information',
+    jp: 'レスポンス情報をリクエストする'
+  },
+  requestProblemInformation: {
+    zh: '请求失败信息',
+    en: 'Request Problem Information',
+    jp: '失敗情報をリクエストする'
+  },
+  topicReuired: {
+    zh: '请输入 Topic',
+    en: 'Topic is required',
+    jp: 'トピックを入力してください'
+  },
+  payloadReuired: {
+    zh: '请输入 Payload',
+    en: 'Payload is required',
+    jp: 'Payloadを入力してください'
+  },
+  color: {
+    zh: '标记',
+    en: 'Color',
+    jp: 'マーク'
+  },
+  willMessage: {
+    zh: '遗嘱消息',
+    en: 'Last Will and Testament',
+    jp: '遺言'
+  },
+  strictValidateCertificate: {
+    zh: '严格证书验证',
+    en: 'Strict validate Certificate',
+    jp: 'SSL証明書'
+  },
+  willTopic: {
+    zh: '遗嘱消息主题',
+    en: 'Last-Will Topic',
+    jp: '遺言トピック'
+  },
+  willPayload: {
+    zh: '遗嘱消息',
+    en: 'Last-Will Payload',
+    jp: '遺言 Payload'
+  },
+  willQos: {
+    zh: '遗嘱消息 QoS',
+    en: 'Last-Will QoS',
+    jp: '遺言 QoS'
+  },
+  willRetain: {
+    zh: '遗嘱消息保留标志',
+    en: 'Last-Will Retain',
+    jp: '遺言 Retain'
+  },
+  willDelayInterval: {
+    zh: '遗嘱消息延迟时间',
+    en: 'Will Delay Interval',
+    jp: '遺言ディレイ間隔'
+  },
+  messageExpiryInterval: {
+    zh: '遗嘱消息过期时间',
+    en: 'Message Expiry Interval',
+    jp: '遺言有効期限'
+  },
+  contentType: {
+    zh: '遗嘱消息描述',
+    en: 'Content Type',
+    jp: '遺言詳細情報'
+  },
+  isUTF8Data: {
+    zh: '是否为 UTF-8 编码数据',
+    en: 'is UTF-8 Encoded Data',
+    jp: 'UTF-8エンコードデータフラグ'
+  },
+  duplicateName: {
+    zh: '该名称已存在，请重新命名！',
+    en: 'Duplicate name. Please rename it!',
+    jp: '名称が既に存在したので、リネームをお願いします！'
+  },
+  nameTip: {
+    zh: '可快速选择已创建过的连接配置',
+    en: 'Quick selection of created connection configurations',
+    jp: '作成された接続構成を早めに選択することができます'
+  },
+}
